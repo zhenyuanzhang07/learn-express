c29d17d798e0318849aa0bf4017002f78db36eb5
Zhenyuan Zhang <zhang.zhenyuan@northeastern.edu>
2024-03-06 15:37:02 -0500

    updated quiz/server.js

--- quiz/server.js
+++ quiz/server.js
@@ -1,46 +1,42 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 const app = express();
-var cors = require('cors');
 const port = 8000;
 
 let users;
-fs.readFile(path.resolve(__dirname, '../data/users.json'), function(err, data) {
-  console.log('reading file ... ');
-  if(err) throw err;
+fs.readFile(path.resolve(__dirname, '../data/users.json'), (err, data) => {
+  if (err) throw err;
   users = JSON.parse(data);
-})
+});
 
-const addMsgToRequest = function (req, res, next) {
-  if(users) {
+const addMsgToRequest = function(req, res, next) {
+  if (users) {
     req.users = users;
     next();
-  }
-  else {
+  } else {
     return res.json({
-        error: {message: 'users not found', status: 404}
+      error: { message: 'users not found', status: 404 }
     });
   }
-  
-}
+};
 
-app.use(
-  cors({origin: 'http://localhost:3000'})
-);
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Endpoint to get usernames
 app.use('/read/usernames', addMsgToRequest);
-
 app.get('/read/usernames', (req, res) => {
-  let usernames = req.users.map(function(user) {
-    return {id: user.id, username: user.username};
+  let usernames = req.users.map(user => {
+    return { id: user.id, username: user.username };
   });
   res.send(usernames);
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Endpoint for adding a new user
 app.use('/write/adduser', addMsgToRequest);
-
 app.post('/write/adduser', (req, res) => {
   let newuser = req.body;
   req.users.push(newuser);
@@ -49,8 +45,19 @@ app.post('/write/adduser', (req, res) => {
     else console.log('User Saved');
   });
   res.send('done');
-})
+});
+
+// New endpoint for searching a user by username and returning their email
+app.get('/read/username/:name', (req, res) => {
+  const { name } = req.params;
+  const user = users.find(user => user.username === name);
+  if (user) {
+    res.json({ email: user.email });
+  } else {
+    res.status(404).json({ error: "User not found" });
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server listening on port ${port}`);
+});
